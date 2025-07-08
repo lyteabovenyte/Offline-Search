27728b2c40bec612ee1b47fd93b189f8f5996d30
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-08 06:58:53 +0330

    crawling through the whole directory in each file and parsing only the characters and text in each xhtml files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use std::fs::{self, File};
 use std::io::{self, Read};
-use std::fs::File;
 use std::process;
 use xml::reader::{EventReader, XmlEvent};
 
@@ -25,11 +25,24 @@ fn read_entire_xml_file(file_path: &str) -> io::Result<String> {
     Ok(content)
 }
 
-fn main() {
-    let file_path = "docs.gl/gl4/glClear.xhtml";
-    let content = read_entire_xml_file(file_path).unwrap_or_else(|err| {
-        eprintln!("Error reading file {}: {}", file_path, err);
+fn main() -> io::Result<()>{
+    let dir_path = "docs.gl/gl4";
+    if !fs::metadata(dir_path).is_ok() {
+        eprintln!("Directory {} does not exist or is not accessible.", dir_path);
+        process::exit(1);
+    }
+    let dir = fs::read_dir(dir_path).unwrap_or_else(|err| {
+        eprintln!("Error reading directory {}: {}", dir_path, err);
         process::exit(1);
     });
-    println!("{}", content);
+
+    for file in dir {
+        let file_path = file?.path();
+        let content = read_entire_xml_file(file_path.to_str().unwrap()).unwrap_or_else(|err| {
+            eprintln!("Error reading file {:?}: {}", file_path, err);
+            process::exit(1);
+        });
+        println!("{}", content);
+    }
+    Ok(())
 }
